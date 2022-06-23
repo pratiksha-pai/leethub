09bd010754226793ef2922e802d44376b6fb76bf
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-23 14:58:04 +0530

    Time: 1193 ms (30.90%), Space: 20.2 MB (29.29%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -1,28 +1,21 @@
 class Solution:
     def scheduleCourse(self, c: List[List[int]]) -> int:
-        q = []
-        heapq.heapify(q)
         n=len(c)
         ct=0 
         c=sorted(c, key = lambda x: x[1])
         i=0
+        p=[]
         o=0
-        # print(c)
         while i<n:
-            # print(c[i][0])
             if ct+c[i][0] <= c[i][1]:
                 ct+=c[i][0]
-                q.append(-1*c[i][0])
+                p.append(c[i][0])
                 o+=1
             else:
-                heapq.heapify(q)
-                if ct!=0 and len(q)!=0:
-                    h=q[0]
-                    if c[i][0]<-1*h and ct+h+c[i][0]<=c[i][1]:
-                        ct+=(c[i][0]+h)
-                        del q[0]
-                        q.append(-1*c[i][0])
-
-            # print(q)
+                p.sort(reverse=True)
+                if not ct==0 and c[i][0]<p[0] and ct-p[0]+c[i][0]<=c[i][1]:
+                    ct+=c[i][0]-p[0]
+                    del p[0]
+                    p.append(c[i][0])
             i+=1
         return o
