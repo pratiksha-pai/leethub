668638a1066886c2aa61e15b8dc0e0fd584b117a
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-23 14:52:03 +0530

    Time: 1114 ms (38.68%), Space: 20 MB (68.42%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -11,14 +11,14 @@ class Solution:
             if ct+c[i][0] <= c[i][1]:
                 ct+=c[i][0]
                 p.append(c[i][0])
-                p.sort(reverse=True)
                 o+=1
             else:
                 # print(c[i][0])
+                p.sort(reverse=True)
                 if not ct==0 and c[i][0]<p[0] and ct-p[0]+c[i][0]<=c[i][1]:
                     ct+=c[i][0]-p[0]
                     del p[0]
                     p.append(c[i][0])
-                    p.sort(reverse=True)
+                    # p.sort(reverse=True)
             i+=1
         return o
