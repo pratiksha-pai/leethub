98750bb9a2875cb42bbae455813b9c2fae568268
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-25 13:41:26 +0530

    Time: 370 ms (13.81%), Space: 15.3 MB (49.84%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        n=len(nums)
+        if n<=2:
+            return True
+        
+        c=0
+        nums.insert(0, -100001)
+        # nums[0]=-1000001, nums[1]=nums[0] hence nums[1]>nums[0] 
+        # test for i>2
+        for i in range(2, n+1):
+            # print('for num '+str(nums[i]))
+            if nums[i]<nums[i-1]:
+                # check if nums[i] > nums[i-2] to decide whether i should be replaced or i-1
+                if nums[i]<nums[i-2]:
+                    nums[i]=nums[i-1]
+                else:
+                    nums[i-1]=nums[i]
+                
+                c+=1
+                # print('c is '+str(c))
+                if c>1:
+                    return False
+            
+        
+        
+        if c>1:
+            return False
+        return True
