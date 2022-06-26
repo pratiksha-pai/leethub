f4892577ca98163d09028eb830383cb34043e7fb
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-26 12:01:41 +0530

    Time: 109 ms (61.13%), Space: 17.2 MB (99.23%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -2,7 +2,6 @@ class Solution:
     def insert(self, a: List[List[int]], b: List[int]) -> List[List[int]]:
         
         n=len(a)
-        # print(n)
         if len(a)==0:
             a.append(b)
             return a
@@ -14,14 +13,12 @@ class Solution:
         
         a.append(b)
         a.sort(key=lambda t: t[0])
-        # print(a)
         i=1
         while i<len(a):
             if a[i][0]<=a[i-1][1]:
                 if a[i-1][1]<=a[i][1]:
                     a[i-1][1]=a[i][1]
                 del a[i]
-                # print(a)
             else:
                 i+=1
             
