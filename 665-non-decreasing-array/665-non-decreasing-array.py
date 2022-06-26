c92dd06865a27199aa161223d513d1304bf9bfeb
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-26 10:39:00 +0530

    Time: 347 ms (22.41%), Space: 15.2 MB (50.00%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -5,12 +5,16 @@ class Solution:
             return True
         
         c=0
-        nums.insert(0, -100001)
+        # nums.insert(0, -100001)
         # nums[0]=-1000001, nums[1]=nums[0] hence nums[1]>nums[0] 
         # test for i>2
-        for i in range(2, n+1):
+        for i in range(1, n):
             # print('for num '+str(nums[i]))
             if nums[i]<nums[i-1]:
+                if i==1:
+                    nums[i-1]=nums[i]
+                    c+=1
+                    continue
                 # check if nums[i] > nums[i-2] to decide whether i should be replaced or i-1
                 if nums[i]<nums[i-2]:
                     nums[i]=nums[i-1]
