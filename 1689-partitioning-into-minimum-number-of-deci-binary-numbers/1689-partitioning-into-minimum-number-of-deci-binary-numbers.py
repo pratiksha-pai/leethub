59dab14e23700c42511215d88ef74566e7978002
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-27 09:30:11 +0530

    Time: 129 ms (50.42%), Space: 15.4 MB (23.95%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -3,11 +3,5 @@ class Solution:
         n=len(s)
         if n==0:
             return 0
-        c=0
-        for i in range(n):
-            # print(ord(s[i]))
-            # print(ord("0"))
-            t=(ord(s[i])-ord("0"))
-            if c<t:
-                  c=t
-        return c
+                
+        return int(max(list(s)))
