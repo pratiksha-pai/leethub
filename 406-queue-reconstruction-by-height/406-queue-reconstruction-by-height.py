0fc669113fb018fbd22f8e5c0db3a30501b71736
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-29 17:56:18 +0530

    Time: 120 ms (75.93%), Space: 14.5 MB (64.95%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reconstructQueue(self, p: List[List[int]]) -> List[List[int]]:
+        p=sorted(p, key=lambda x: (-x[0], x[1]))
+
+        o = []
+        for i in p:
+            o.insert(i[1], i)
+        return o
+        print(p)
+        
+        return o
