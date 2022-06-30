17f000b599d2190590bff8ed1936dca382e1d183
Pratiksha Pai <paipratisha8@gmail.com>
2022-06-30 09:58:58 +0530

    Time: 115 ms (49.43%), Space: 15.3 MB (79.18%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -3,12 +3,9 @@ class Solution:
         nums.sort()
         # find median and subtract
         n=len(nums)
-        t1=t2=0
-        t1=nums[int(n/2)]
-        # t2=nums[int(n/2)+1]
-        s1=0
+        t=nums[int(n/2)]
+        s=0
         for i in range(n):
-            s1+=abs(nums[i]-t1)
-            # s2+=abs(nums[i]-t2)
+            s+=abs(nums[i]-t)
         
-        return s1
+        return s
