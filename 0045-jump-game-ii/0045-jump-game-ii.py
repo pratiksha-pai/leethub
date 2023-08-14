a80bcc01bf462828b37ff7e0e4a575005d93c7c5
Pratiksha Pai <paipratisha8@gmail.com>
2023-08-13 22:00:01 -0400

    Time: 154 ms (40.10%), Space: 17.4 MB (47.70%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,8 +6,9 @@ class Solution:
         dp = [(2**53)-1 for _ in range(n)]
         dp[0] = 0
         for i in range(n):
+            max_from_before = max_from_here
             max_from_here = max(max_from_here, nums[i]+i)
-            for j in range(i+1, min(max_from_here+1, n)):
+            for j in range(max(i+1, max_from_before), min(max_from_here+1, n)):
                 dp[j] = min(1+dp[i], dp[j])
                 
         return dp[n-1] 
