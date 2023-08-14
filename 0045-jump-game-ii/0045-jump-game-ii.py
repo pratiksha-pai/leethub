eec9d72b44b335ca0e4d441861dd2195d5fffc53
Pratiksha Pai <paipratisha8@gmail.com>
2023-08-13 21:58:36 -0400

    Time: 7249 ms (12.71%), Space: 17.4 MB (47.70%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        import sys
+        max_from_here = 0
+        n = len(nums)
+        dp = [(2**53)-1 for _ in range(n)]
+        dp[0] = 0
+        for i in range(n):
+            max_from_here = max(max_from_here, nums[i]+i)
+            for j in range(i+1, min(max_from_here+1, n)):
+                dp[j] = min(1+dp[i], dp[j])
+                
+        return dp[n-1] 
