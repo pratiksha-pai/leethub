b9446696e3d97538f498cf7f45b48f192bc0fa18
Pratiksha Pai <paipratisha8@gmail.com>
2024-02-01 21:45:30 -0500

    Time: 75 ms (71.53%), Space: 18.2 MB (89.50%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -3,7 +3,6 @@ class Solution:
         
         n = len(g)
         res = []
-        visit = set()
         
         def dfs(node, path):
             if node == n-1:
@@ -11,12 +10,10 @@ class Solution:
                 return
             
             for child in g[node]:
-                if child not in visit:
-                    visit.add(child)
-                    path.append(child)
-                    dfs(child, path)
-                    visit.remove(child)
-                    path.pop()
+                dfs(child, path + [child])
+        
+        
+        
         dfs(0, [0])
         return res
             
