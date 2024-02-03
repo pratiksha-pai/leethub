460b01cae98f8e1a2e172a7babb059e27dabd784
Pratiksha Pai <paipratisha8@gmail.com>
2024-02-03 13:52:53 -0500

    Time: 4483 ms (54.59%), Space: 16.6 MB (58.87%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        n = len(word)
+        R, C = len(board), len(board[0])
+        visited = set()
+        
+        def dfs(i, x, y):
+            if i == n:
+                return True
+            
+            if x < 0 or y < 0 or x >= R or y >= C or board[x][y] != word[i] or (x, y) in visited:
+                return False
+            visited.add((x, y))
+            
+            for dx, dy in dirs:
+                nx, ny = x+dx, y+dy
+                if dfs(i+1, nx, ny):
+                    return True
+            visited.remove((x, y))
+            
+            return False
+        
+        for i in range(R):
+            for j in range(C):
+                if dfs(0, i, j):
+                    return True
+        
+        return False
+            
+        
+        
+                
+            
+        
