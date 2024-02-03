22b7a93a6d18dd095b29f43e4bdc6838d8fc62db
Pratiksha Pai <paipratisha8@gmail.com>
2024-02-03 18:31:25 -0500

    Time: 754 ms (28.34%), Space: 25.1 MB (84.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -4,11 +4,8 @@ class Solution:
         
         dp = [[0 for _ in range(C+1)] for _ in range(R+1)]
         
-        for i in range(R+1):
-            for j in range(C+1):
-                if i == 0 or j == 0:
-                    dp[i][j] = 0
-                    continue
+        for i in range(1, R+1):
+            for j in range(1, C+1):
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
                 if a[i-1] == b[j-1]:
                     dp[i][j] = max(dp[i][j], 1+dp[i-1][j-1])
