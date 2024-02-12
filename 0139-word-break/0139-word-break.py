add6f2a51e67c0ae97dab90100f6a5ef9e6ea67e
Pratiksha Pai <paipratisha8@gmail.com>
2024-02-11 20:00:59 -0500

    Time: 43 ms (50.79%), Space: 16.7 MB (64.82%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -6,17 +6,20 @@ class Solution:
         
         def dfs(start):
             if start == len(s):
-                return True 
+                return True
+            
             if start in memo:
                 return memo[start]
             
             for end in range(start+1, len(s)+1):
-                if s[start:end] in wd and dfs(end):
-                    memo[start] = True
-                    return True
+                segment = s[start:end]
+                if segment in wd:
+                    if dfs(end):
+                        memo[start] = True
+                        return True
             
             memo[start] = False
             return False
-
         
         return dfs(0)
+                
