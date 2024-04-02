6f23d6057ac5422778b751978af0260279997fa4
Pratiksha Pai <paipratisha8@gmail.com>
2024-04-02 16:47:14 -0400

    Time: 3568 ms (75.82%), Space: 16.5 MB (71.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,24 +3,22 @@ class Solution:
         
 
         n = len(word)
-        # seen = set()
         dirs = [[-1, 0], [0, -1], [1, 0], [0, 1]]
         
-        def dfs(path, i, x, y):
+        def dfs(i, x, y):
             if i == n:
-                if ''.join(path) == word:
+                return True
+            
+            if x < 0 or x >= R or y < 0 or y >= C or b[x][y] != word[i]:
+                return False
+            
+            tmp, b[x][y] = b[x][y], '#'
+            
+            for dx, dy in dirs:
+                nx, ny = x+dx, y+dy
+                if dfs(i+1, nx, ny):
                     return True
-                else:
-                    return False
-            
-            
-            if 0<=x<R and 0<=y<C and b[x][y] == word[i] and (x, y) not in seen:
-                seen.add((x, y))
-                for dx, dy in dirs:
-                    nx, ny = x+dx, y+dy
-                    if dfs(path + [word[i]], i+1, nx, ny):
-                        return True
-                seen.remove((x, y))
+            b[x][y] = tmp 
             
             return False
         
@@ -28,10 +26,8 @@ class Solution:
         
         for x in range(R):
             for y in range(C):
-                if word[0] == b[x][y]:
-                    seen = set()
-                    if dfs([], 0, x, y):
-                        return True
+                if word[0] == b[x][y] and dfs(0, x, y):
+                    return True
         
         return False
                 
