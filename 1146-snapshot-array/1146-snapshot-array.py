45c59f8e021e035afbfff90fd2f5393473d5bc2e
Pratiksha Pai <paipratisha8@gmail.com>
2024-05-07 18:27:56 -0400

    Time: 6738 ms (5.06%), Space: 41.6 MB (96.53%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,17 +1,17 @@
 class SnapshotArray:
-    from collections import defaultdict
+
     def __init__(self, length: int):
         self.arr = defaultdict(int)
         self.snap_id = 0
 
     def set(self, index: int, val: int) -> None:
         self.arr[(index, self.snap_id)] = val
-            
+
     def snap(self) -> int:
         self.snap_id += 1
-        return self.snap_id - 1
+        return self.snap_id -1
 
-    def get(self, index: int, snap_id: int) -> int:        
+    def get(self, index: int, snap_id: int) -> int:
         while (index, snap_id) not in self.arr and snap_id >= 0:
             snap_id -= 1
         return self.arr[(index, snap_id)]
