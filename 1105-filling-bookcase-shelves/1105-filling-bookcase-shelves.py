1626aca42c6169caff3f4417a112aedcb3f9b2c0
Pratiksha Pai <paipratisha8@gmail.com>
2024-05-07 17:12:14 -0400

    Time: 44 ms (93.33%), Space: 16.9 MB (76.67%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -1,11 +1,15 @@
 class Solution:
-    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        dp = [0] + [float('inf')] * len(books)
-        for i in range(1, len(books) + 1):
-            width, height, j = 0, 0, i
-            while j > 0 and width + books[j - 1][0] <= shelfWidth:
-                width += books[j - 1][0]
-                height = max(height, books[j - 1][1])
-                dp[i] = min(dp[i], dp[j - 1] + height)
-                j -= 1
-        return dp[-1]
+    def minHeightShelves(self, books: List[List[int]], sw: int) -> int:
+        n = len(books)
+        dp = [0] + [float('inf') for _ in range(n)]
+
+        for i in range(1, n+1):
+            w, h, j = 0, 0, i-1
+            while j >=0 and w+books[j][0] <= sw:
+                w+=books[j][0]
+                h = max(h, books[j][1])
+                dp[i] = min(dp[i], h + dp[j])
+                j-=1
+        
+        return dp[n]
+                
