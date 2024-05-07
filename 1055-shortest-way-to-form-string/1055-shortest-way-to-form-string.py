768a95a31544dba5e4a7b53a94e718981f7c2fc1
Pratiksha Pai <paipratisha8@gmail.com>
2024-05-07 16:26:21 -0400

    Time: 49 ms (43.55%), Space: 16.5 MB (97.01%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,20 +1,20 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        def subsequenceIndex(source, target):
-            si = ti = 0
-            while si < len(source) and ti < len(target):
-                if source[si] == target[ti]:
-                    ti += 1
-                si += 1
-            return ti   
-
-        count = 0
+        def get_target_index(s, t):
+            si, ti = 0, 0
+            while si < len(s) and ti < len(t):
+                if s[si] == t[ti]:
+                    ti+=1
+                si+=1
+            
+            return ti
+        
         i = 0
-        while i < len(target):
-            pos = subsequenceIndex(source, target[i:])
+        count = 0
+        while i<len(target):
+            pos = get_target_index(source, target[i:])
             if pos == 0:
                 return -1
             i += pos
-            count += 1
+            count +=1
         return count
-    
